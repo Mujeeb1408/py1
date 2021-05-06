fa4c72897d6bb79008107bca7cb285845b05ffb1
Mujeeb1408 <83758082+Mujeeb1408@users.noreply.github.com>
2021-05-06 16:33:37 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,7 @@
+# Area of triangle
+
+h = float(input("Enter the height of triangle: "))
+b = float(input("Enter the base of the triangle:"))
+Area = (h*b)/2
+print("Area of triangle :")
+print(Area)
---
+++ test3.py
@@ -0,0 +1,5 @@
+# Area of square
+h = float(input("Enter the hieght of Square: "))
+Area = h**2
+print("Area of square :")
+print(Area)
