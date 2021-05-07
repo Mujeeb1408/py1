1f0be9b9011e4c4586414644a4d74faaff9f28ff
Mujeeb1408 <83758082+Mujeeb1408@users.noreply.github.com>
2021-05-07 05:37:38 +0530

    Update avgoftest.py

--- avgoftest.py
+++ avgoftest.py
@@ -6,3 +6,13 @@ c = float(input())
 avg = (a+b+c)/3
 print('Average of the test is')
 print(avg)
+#calculating the best of 2 average
+if a < b and a < c:
+    avg = (b+c)/2
+    print('Best of 2 average is' + str(avg))
+elif b < c:
+    avg = (a+c) / 2
+    print('Best of 2 average is' + str(avg))
+else:
+    avg = (a+b) / 2
+    print('Best of 2 average is' + str(avg))
